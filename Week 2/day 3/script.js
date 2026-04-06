5e045caa09fa17ddf50cf87970a54355f1855e10
Adhish-hub <adish.tiwari@startappss.com>
2026-04-06 11:46:49 +0530

    Day 3 dark mode

--- Week 2/day 3/script.js
+++ Week 2/day 3/script.js
@@ -1,30 +1,27 @@
-const button = document.getElementById("toggle-btn");
-const statusText = document.getElementById("status");
+let darkmode = localStorage.getItem('darkmode')
+const changeTheme = document.getElementById('change-theme')
+const themeStatus = document.getElementById('status')
 
-
-let currentTheme = localStorage.getItem("theme") || "light";
-document.body.classList.add(currentTheme);
-updateUI();
-
-
-button.addEventListener("click", () => {
-  document.body.classList.toggle("dark");
-  document.body.classList.toggle("light");
-
-  currentTheme = document.body.classList.contains("dark") ? "dark" : "light";
-
-  localStorage.setItem("theme", currentTheme);
-
-  updateUI();
-});
-
-
-function updateUI() {
-  if (currentTheme === "dark") {
-    button.textContent = "Light Mode";
-    statusText.textContent = "Current Theme: Dark";
-  } else {
-    button.textContent = "Dark Mode";
-    statusText.textContent = "Current Theme: Light";
-  }
+const enableDarkmode = () => {
+    document.body.classList.add('darkmode')
+    localStorage.setItem('darkmode', 'active')
+    themeStatus.textContent = "dark mode"
 }
+
+const disableDarkmode = () => {
+    document.body.classList.remove('darkmode')
+    localStorage.removeItem('darkmode')
+    themeStatus.textContent = "light mode"
+}
+
+if(darkmode === "active") {
+    enableDarkmode()
+} else{
+    disableDarkmode()
+}
+
+changeTheme.addEventListener("click", () => {
+    darkmode = localStorage.getItem('darkmode')
+    darkmode !== "active" ? enableDarkmode() : disableDarkmode()
+})
+
